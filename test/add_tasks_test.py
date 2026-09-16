f09fdc1f1e7189fa402f0f58327a030f1b9a3db9
Sergey <serfre13@gmail.com>
2026-09-17 00:52:40 +0700

    Добавлен тест на добавление и редактирование

---
+++ test/add_tasks_test.py
@@ -0,0 +1,17 @@
+import requests
+
+# создать задачу, изменить и проверить что ИД не поменялся
+def add_tasks():
+    url = "http://5.101.50.9:8014/"
+    body = {"title":"задание на сегодня","completed":False}
+    response = requests.post(url, json = body)
+    response_body_id = response.json()[id]
+    assert response.status_code == 200
+
+#изменить и проверить что ИД не поменялся
+    body = {"title":"Новое задание"}
+    response = requests.patch(f'{url}/{response_body_id}', json = body)
+    response_id = response.json()[id]
+    assert  response_body_id == response_id
+
+
